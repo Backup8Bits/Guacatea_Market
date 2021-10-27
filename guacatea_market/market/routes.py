7fe69c93da9b09373222fbf5b5aa5c8f85c5d093
JEMV <emiliomontesv@hotmail.com>
2021-10-27 15:06:48 -0500

    Login

--- guacatea_market/market/routes.py
+++ guacatea_market/market/routes.py
@@ -5,3 +5,7 @@ from flask import render_template
 @app.route('/home')
 def home_page():
     return render_template('home.html')
+
+@app.route("/login")
+def login_page():
+    return render_template("login.html")
