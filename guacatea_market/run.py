80095e1864f9d23f9498e6f5c1e352c8bfd1bea9
neimv <neimv.zatara@gmail.com>
2021-10-27 17:17:41 -0500

    create docker and docker-compose

--- guacatea_market/run.py
+++ guacatea_market/run.py
@@ -1,5 +1,20 @@
+
+# Core libraries
+import os
+
+# Third libraries
+from dotenv import load_dotenv
+
+# owner libraries
 from market import app
+
+
+load_dotenv()
 
 # Checa si el run.py ha sido ejecutado directamente y no importado
 if __name__ == '__main__':
-    app.run(host="localhost", port=8000, debug=True)
+    PORT = int(os.getenv("FLASK_RUN_PORT", 5000))
+    HOST = os.getenv("FLASK_RUN_HOST", "0.0.0.0")
+    DEBUG = os.getenv("DEBUG", False)
+
+    app.run(host=HOST, port=PORT, debug=DEBUG)
