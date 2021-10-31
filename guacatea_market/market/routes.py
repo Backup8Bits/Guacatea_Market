73bfe29fd3f30c9dfc448f0448cbbd0f3cbfe271
Fernando <90936639+Fer-Bar@users.noreply.github.com>
2021-10-31 15:37:31 -0400

    Update routes.py
    Actualize las rutas para que puedan desarrollarlas

--- guacatea_market/market/routes.py
+++ guacatea_market/market/routes.py
@@ -7,6 +7,29 @@ from flask import render_template
 def home_page():
     return render_template('home.html')
 
+
 @app.route('/login')
 def home_login():
     return render_template('login.html')
+
+
+@app.route('/market')
+def market_page():
+    return render_template("market.html")
+
+
+@app.route('/register', methods=["GET","POST"])
+def register_page():
+    return render_template('register.html')
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login_page():
+    return render_template('login.html')
+
+
+"""
+@app.route("/logout")
+def logout_page():
+    return redirect(url_for('home_page'))
+"""
