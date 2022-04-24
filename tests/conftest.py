0e966ee1a8621e05754ff528d7e8d5f270ef1aef
Fer-Bar <linofernando2703@gmail.com>
2022-04-24 15:26:38 -0400

    Changes in dockerfile for wsgi server

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,6 @@ import datetime
 
 import pytest
 
-from web.market import app, db
 from web.market.models.item import Item
 from web.market.models.user import User
 
@@ -23,39 +22,3 @@ def new_item():
                 date_format= datetime.datetime.now()
                )
     return item
-
-# Necesito una instancia prueba de la aplicación Flask para realizar estos tests
-
-# @pytest.fixture(scope='module')
-# def test_client():
-#     flask_app = create_app('flask_test.cfg')
-
-#     # Create a test client using the Flask application configured for testing
-#     with flask_app.test_client() as testing_client:
-#         # Establish an application context
-#         with flask_app.app_context():
-#             yield testing_client  # this is where the testing happens!
-
-# @pytest.fixture(scope='module')
-# def init_database(test_client):
-#     # Create the database and the database table
-#     db.create_all()
-
-#     # Insert user data
-#     user1 = User(username='FlaskUser',
-#                 email='patkennedy79@gmail.com',
-#                 password='KennedyRules')
-
-#     user2 = User(username='SecondUser',
-#                 email='something12@gmail.com',
-#                 password='PaSsWoRd')
-
-#     db.session.add(user1)
-#     db.session.add(user2)
-
-#     # Commit the changes for the users
-#     db.session.commit()
-
-#     yield  # this is where the testing happens!
-
-#     db.drop_all()
