59a2207f6f4605b69b3ae67102bd36f94d2c6d98
Fer-Bar <linofernando2703@gmail.com>
2022-07-06 22:41:50 -0400

    Fixed firebase connection

--- web/market/firebase_connection.py
+++ web/market/firebase_connection.py
@@ -1,21 +1,26 @@
+import os
 import secrets
 
 import firebase_admin
+from dotenv import load_dotenv
 from firebase_admin import credentials, storage
 from PIL import Image
 
+load_dotenv()
+
 cred = credentials.Certificate("/code/market/serviceAccountKey.json")
-app = firebase_admin.initialize_app(cred, {"storageBucket": "guacatea-market.appspot.com"})
+app = firebase_admin.initialize_app(cred, {"storageBucket": "g-market-5f76f.appspot.com"})
 
 def upload_file(image):
-    picture_path = secrets.token_hex(12)
+    random_path = secrets.token_hex(12)
     bucket = storage.bucket()
     img = Image.open(image)
+    img = img.convert('RGB')
     img.thumbnail((800, 800))
-    resize_img = img.save(picture_path + '.' + img.format)
-    path = picture_path + '.' + img.format
-    blob = bucket.blob(str(resize_img))
-    blob.upload_from_filename(path)
+    filename = random_path + '.' + 'png'
+    img.save(filename) # Save format
+    blob = bucket.blob(str(filename))
+    blob.upload_from_filename(filename)
     blob.make_public()
-    print("your file url", blob.public_url)
+    print("Your file url", blob.public_url)
     return blob.public_url
